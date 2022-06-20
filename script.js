3c9fe27984b84b897fd4cfd5d0f4c9d3bc9a035f
Rosângela Nery <rosangela2nery@gmail.com>
2022-06-20 14:40:16 -0300

    Funcionalidade para escolher a quantidade de cartas

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
-let pergunta = Number(prompt(`Quantas cartas você quer jogar?
+let quantidadeCartas = 0
+
+function pegarQuantidade() {
+    let numeroDeCartas = Number(prompt(`Quantas cartas você quer jogar? 
 Obs: só poderá inserir números pares, de 4 a 14.`));
 
+    if (numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 !== 0) {
+        pegarQuantidade();
+    }
+    else {
+        quantidadeCartas = numeroDeCartas;
+    }
+}
+
+pegarQuantidade();
 
 let galeria = document.querySelector(".galeriaDeCartas");
 let images = [
-    "1.gif", 
-    "2.gif", 
-    "3.gif", 
-    "4.gif", 
-    "5.gif", 
-    "6.gif", 
+    "1.gif",
+    "2.gif",
+    "3.gif",
+    "4.gif",
+    "5.gif",
+    "6.gif",
     "7.gif"
 ];
 
 let cartaHTML = "";
 
-images.forEach(img => {
-  cartaHTML += `
-    <div class="memoria-carta" data-carta="${img}">
+for (let index = 0; index < quantidadeCartas/2; index++) {
+    cartaHTML += `
+    <div class="memoria-carta" data-carta="${images[index]}">
         <div class="front-face">
             <img class="verso" src="imagens/front.png"/>
         </div>
         <div class="back-face">
-            <img class="frente" src="imagens/${img}"/>
+            <img class="frente" src="imagens/${images[index]}"/>
         </div>
     </div>
   `;
-});
+}
 
 galeria.innerHTML = cartaHTML + cartaHTML;
 
 let cartas = document.querySelectorAll(".memoria-carta");
-let primeiraCarta, segundaCarta;
+let primeiraCarta = 0;
+let segundaCarta = 0;
 let cartaNova = false;
 let corresponde = false;
 
 function virarCarta () {
-   
-    if(cartaNova) return false;
-    this.classList.add('virar');
 
-    if(!primeiraCarta) {
-        primeiraCarta = this;
-        return false;
+    let carta_clicada = this.classList.contains("virar");
+
+    if (carta_clicada === false) {
+        if(cartaNova) return false;
+        this.classList.add('virar');
+
+        if(!primeiraCarta) {
+            primeiraCarta = this;
+            return false;
+        }
+        segundaCarta = this;
+
+        cartaCorrenpondia();
     }
-    segundaCarta = this;
-
-    cartaCorrenpondia();
 }
 
 function cartaCorrenpondia() {
